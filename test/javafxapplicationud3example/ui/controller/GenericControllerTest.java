1cfad08a29674d5aa6e898fc5bbcc697d7277cbd
jmarturi <jmarturi@HZ059363.TartangaLH.eus>
2018-10-25 08:34:17 +0200

    Junit test class added for GenericController

---
+++ test/javafxapplicationud3example/ui/controller/GenericControllerTest.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javafxapplicationud3example.ui.controller;
+
+import javafx.stage.Stage;
+import javafxapplicationud3example.businessLogic.UsersManager;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author jmarturi
+ */
+public class GenericControllerTest {
+    
+    public GenericControllerTest() {
+    }
+
+    /**
+     * Test of setUsersManager method, of class GenericController.
+     */
+    @Test
+    public void testSetUsersManager() {
+        System.out.println("setUsersManager");
+        UsersManager usersManager = null;
+        GenericController instance = new GenericController();
+        instance.setUsersManager(usersManager);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getStage method, of class GenericController.
+     */
+    @Test
+    public void testGetStage() {
+        System.out.println("getStage");
+        GenericController instance = new GenericController();
+        Stage expResult = null;
+        Stage result = instance.getStage();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setStage method, of class GenericController.
+     */
+    @Test
+    public void testSetStage() {
+        System.out.println("setStage");
+        Stage stage = null;
+        GenericController instance = new GenericController();
+        instance.setStage(stage);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of showErrorAlert method, of class GenericController.
+     */
+    @Test
+    public void testShowErrorAlert() {
+        System.out.println("showErrorAlert");
+        String errorMsg = "";
+        GenericController instance = new GenericController();
+        instance.showErrorAlert(errorMsg);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
